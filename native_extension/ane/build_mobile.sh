3e21134b39afb5af6411de9baa8206aee6de40a1
Eoin Landy <el111@yahoo.com>
2017-03-28 00:08:51 +0100

    0.0.16 Android initial commit

---
+++ native_extension/ane/build_mobile.sh
@@ -0,0 +1,108 @@
+#!/bin/sh
+
+#Get the path to the script and trim to get the directory.
+echo "Setting path to current directory to:"
+pathtome=$0
+pathtome="${pathtome%/*}"
+
+
+echo $pathtome
+
+PROJECTNAME=WebViewANE
+fwSuffix="_FW"
+libSuffix="_LIB"
+
+AIR_SDK="/Users/User/sdks/AIR/AIRSDK_25"
+echo $AIR_SDK
+
+#Setup the directory.
+echo "Making directories."
+
+if [ ! -d "$pathtome/platforms" ]; then
+mkdir "$pathtome/platforms"
+fi
+if [ ! -d "$pathtome/platforms/ios" ]; then
+mkdir "$pathtome/platforms/ios"
+fi
+if [ ! -d "$pathtome/platforms/ios/simulator" ]; then
+mkdir "$pathtome/platforms/ios/simulator"
+fi
+if [ ! -d "$pathtome/platforms/ios/simulator/Frameworks" ]; then
+mkdir "$pathtome/platforms/ios/simulator/Frameworks"
+fi
+if [ ! -d "$pathtome/platforms/ios/device" ]; then
+mkdir "$pathtome/platforms/ios/device"
+fi
+if [ ! -d "$pathtome/platforms/ios/device/Frameworks" ]; then
+mkdir "$pathtome/platforms/ios/device/Frameworks"
+fi
+if [ ! -d "$pathtome/platforms/ios/default" ]; then
+mkdir "$pathtome/platforms/ios/default"
+fi
+if [ ! -d "$pathtome/platforms/android" ]; then
+mkdir "$pathtome/platforms/android"
+fi
+
+
+#Copy SWC into place.
+echo "Copying SWC into place."
+cp "$pathtome/../bin/$PROJECTNAME.swc" "$pathtome/"
+
+#Extract contents of SWC.
+echo "Extracting files form SWC."
+unzip "$pathtome/$PROJECTNAME.swc" "library.swf" -d "$pathtome"
+
+#Copy library.swf to folders.
+echo "Copying library.swf into place."
+cp "$pathtome/library.swf" "$pathtome/platforms/ios/simulator"
+cp "$pathtome/library.swf" "$pathtome/platforms/ios/device"
+cp "$pathtome/library.swf" "$pathtome/platforms/ios/default"
+cp "$pathtome/library.swf" "$pathtome/platforms/android"
+
+#Copy ios native libraries into place.
+echo "Copying native libraries into place."
+cp -R -L "$pathtome/../../native_library/ios/$PROJECTNAME/Build/Products/Release-iphonesimulator/lib$PROJECTNAME$libSuffix.a" "$pathtome/platforms/ios/simulator/lib$PROJECTNAME.a"
+cp -R -L "$pathtome/../../native_library/ios/$PROJECTNAME/Build/Products/Release-iphoneos/lib$PROJECTNAME$libSuffix.a" "$pathtome/platforms/ios/device/lib$PROJECTNAME.a"
+
+cp -R -L "$pathtome/../../native_library/ios/$PROJECTNAME/Build/Products/Release-iphonesimulator/FRESwift.framework" "$pathtome/platforms/ios/simulator/Frameworks"
+cp -R -L "$pathtome/../../native_library/ios/$PROJECTNAME/Build/Products/Release-iphoneos/FRESwift.framework" "$pathtome/platforms/ios/device/Frameworks"
+
+cp -R -L "$pathtome/../../native_library/ios/$PROJECTNAME/Build/Products/Release-iphonesimulator/$PROJECTNAME$fwSuffix.framework" "$pathtome/platforms/ios/simulator/Frameworks"
+cp -R -L "$pathtome/../../native_library/ios/$PROJECTNAME/Build/Products/Release-iphoneos/$PROJECTNAME$fwSuffix.framework" "$pathtome/platforms/ios/device/Frameworks"
+
+#Copy android native libraries into place.
+
+echo "COPYING Android aars into place"
+cp "$pathtome/../../native_library/android/$PROJECTNAME/app/build/outputs/aar/app-release.aar" "$pathtome/platforms/android/app-release.aar"
+
+echo "getting Android jars"
+unzip "$pathtome/platforms/android/app-release.aar" "classes.jar" -d "$pathtome/platforms/android"
+
+
+
+#Run the build command.
+echo "Generating ANE."
+"$AIR_SDK"/bin/adt -package \
+-target ane "$pathtome/$PROJECTNAME-mobile.ane" "$pathtome/extension_mobile.xml" \
+-swc "$pathtome/$PROJECTNAME.swc" \
+-platform Android-ARM \
+-C "$pathtome/platforms/android" "library.swf" "classes.jar" \
+-platformoptions "platforms/android/platform.xml" "res/values/strings.xml" \
+-platform Android-x86 \
+-C "$pathtome/platforms/android" "library.swf" "classes.jar" \
+-platformoptions "platforms/android/platform.xml" "res/values/strings.xml" \
+-platform iPhone-x86  -C "$pathtome/platforms/ios/simulator" "library.swf" "lib$PROJECTNAME.a" \
+-platformoptions "$pathtome/platforms/ios/platform.xml" \
+-C $pathtome/platforms/ios/simulator/Frameworks/ . \
+-platform iPhone-ARM  -C "$pathtome/platforms/ios/device" "library.swf" "lib$PROJECTNAME.a" \
+-platformoptions "$pathtome/platforms/ios/platform.xml" \
+-C $pathtome/platforms/ios/device/Frameworks/ . 
+
+
+rm -r "$pathtome/platforms/ios/default"
+rm "$pathtome/$PROJECTNAME.swc"
+rm "$pathtome/library.swf"
+rm "$pathtome/platforms/android/library.swf"
+rm "$pathtome/platforms/android/classes.jar"
+
+echo "DONE!"
---
+++ native_extension/src/com/tuarua/webview/AndroidSettings.as
@@ -0,0 +1,31 @@
+/*
+ * Copyright Tua Rua Ltd. (c) 2017.
+ */
+
+/**
+ * Created by Eoin Landy on 26/03/2017.
+ */
+package com.tuarua.webview {
+public class AndroidSettings {
+
+    /**
+     * <p>Sets whether the WebView requires a user gesture to play media.</p>
+     */
+    public var mediaPlaybackRequiresUserGesture:Boolean = true;
+    /**
+     * <p>A Boolean value indicating whether JavaScript is enabled.</p>
+     */
+    public var javaScriptEnabled:Boolean = true;
+    /**
+     * <p>A Boolean value indicating whether JavaScript can open windows without user interaction.</p>
+     */
+    public var javaScriptCanOpenWindowsAutomatically:Boolean = true;
+    /**
+     * <p>A Boolean value indicating whether the WebView should not load image resources from the network.</p>
+     */
+    public var blockNetworkImage:Boolean = false;
+
+    public function AndroidSettings() {
+    }
+}
+}
