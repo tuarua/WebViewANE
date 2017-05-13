6d7b132a7e4a16edd54b1760e002b6a254cbb3cd
Eoin Landy <el111@yahoo.com>
2017-05-13 15:10:56 +0100

    0.0.22 with FreSharp 1.0.6

--- native_library/win/WebViewANE/CefSharpLib/CefSharpLib/FreSharpController.cs
+++ native_library/win/WebViewANE/CefSharpLib/CefSharpLib/FreSharpController.cs
@@ -1,22 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using FreSharp;
+using TuaRua.FreSharp;
 using FREObject = System.IntPtr;
 using FREContext = System.IntPtr;
 
 namespace CefSharpLib {
     public abstract class FreSharpController {
         public Dictionary<string, Func<FREContext, uint, FREObject[], FREObject>> FunctionsDict;
-        public static readonly FreSharpHelper FreHelper = new FreSharpHelper();
-
+        public static FreContextSharp Context;
         public FREObject CallSharpFunction(string name, ref FREContext ctx, uint argc, FREObject[] argv) {
             return FunctionsDict[name].Invoke(ctx, argc, argv);
         }
+
         public void SetFreContext(ref FREContext freContext) {
-            FreHelper.SetFreContext(ref freContext);
+            Context = new FreContextSharp(freContext);
         }
+
         public void Trace(string value) {
-            FreHelper.DispatchEvent("TRACE", value);
+            Context.DispatchEvent("TRACE", value);
         }
+
     }
 }
