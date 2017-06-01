f4c343a1d1743ea6f56b1a89ce2fc517f6bc1a49
Eoin Landy <el111@yahoo.com>
2017-06-01 22:21:39 +0100

    updating License, additional terms.

--- native_library/source/WebViewANE.swift
+++ native_library/source/WebViewANE.swift
@@ -1,16 +1,23 @@
-/*Copyright 2017 Tua Rua Ltd.
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.*/
+// Copyright 2017 Tua Rua Ltd.
+// 
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+// 
+//  http://www.apache.org/licenses/LICENSE-2.0
+// 
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// 
+//  Additional Terms
+//  No part, or derivative of this Air Native Extensions's code is permitted 
+//  to be sold as the basis of a commercially packaged Air Native Extension which 
+//  undertakes the same purpose as this software. That is, a WebView for Windows, 
+//  OSX and/or iOS and/or Android.
+//  All Rights Reserved. Tua Rua Ltd.
 
 import Foundation
 import WebKit
