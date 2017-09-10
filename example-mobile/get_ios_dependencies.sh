6acee9e716ffe5d23990336d7d2f7834cd1434b9
Eoin Landy <el111@yahoo.com>
2017-09-10 18:31:33 +0100

    0.0.31 ready for merge

--- example-mobile/get_ios_dependencies.sh
+++ example-mobile/get_ios_dependencies.sh
@@ -3,3 +3,7 @@
 wget https://github.com/tuarua/Swift-IOS-ANE/releases/download/1.1.0/ios_dependencies.zip
 unzip -u ios_dependencies.zip
 rm ios_dependencies.zip
+
+wget https://github.com/tuarua/WebViewANE/releases/download/0.0.31/ios_dependencies.zip
+unzip -u -o ios_dependencies.zip
+rm ios_dependencies.zip
