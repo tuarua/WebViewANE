e5319d1d8fa2f746312ec98bc6d27014a5e080ec
Eoin Landy <el111@yahoo.com>
2018-01-30 20:01:08 +0000

    1.3.0

--- native_library/source/swift/Settings.swift
+++ native_library/source/swift/Settings.swift
@@ -1,4 +1,4 @@
-// Copyright 2017 Tua Rua Ltd.
+// Copyright 2018 Tua Rua Ltd.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
