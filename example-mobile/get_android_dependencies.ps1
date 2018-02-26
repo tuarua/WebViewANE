0e711c57c250578d75cb0d6f9c19498d6a45aaa2
Eoin Landy <el111@yahoo.com>
2018-02-26 21:39:02 +0000

    github downloads needs TLS1.2

--- example-mobile/get_android_dependencies.ps1
+++ example-mobile/get_android_dependencies.ps1
@@ -1,2 +1,3 @@
 $currentDir = (Get-Item -Path ".\" -Verbose).FullName
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -OutFile "$currentDir\..\native_extension\ane\mobile\com.tuarua.frekotlin.ane" -Uri https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/kotlin/com.tuarua.frekotlin.ane?raw=true
--- example/get_dependencies.ps1
+++ example/get_dependencies.ps1
@@ -1,3 +1,3 @@
 $currentDir = (Get-Item -Path ".\" -Verbose).FullName
-
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -Uri https://github.com/tuarua/Swift-OSX-ANE/releases/download/2.2.0/CommonDependencies.ane?raw=true -OutFile "$currentDir\..\native_extension/ane/CommonDependencies.ane"
