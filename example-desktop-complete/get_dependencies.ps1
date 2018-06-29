ba6503ec5d338473728cd21d70eb8992bdf61ced
Eoin Landy <el111@yahoo.com>
2018-06-29 19:46:36 +0100

    1.7.0

--- example-desktop-complete/get_dependencies.ps1
+++ example-desktop-complete/get_dependencies.ps1
@@ -1,4 +1,4 @@
 $currentDir = (Get-Item -Path ".\" -Verbose).FullName
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -Uri https://github.com/tuarua/Swift-IOS-ANE/releases/download/2.4.0/CommonDependencies.ane?raw=true -OutFile "$currentDir\..\native_extension/ane/CommonDependencies.ane"
-Invoke-WebRequest -Uri https://github.com/tuarua/WebViewANE/releases/download/1.6.5/WebViewANE.ane?raw=true -OutFile "$currentDir\..\native_extension\ane\WebViewANE.ane"
+Invoke-WebRequest -Uri https://github.com/tuarua/WebViewANE/releases/download/1.7.0/WebViewANE.ane?raw=true -OutFile "$currentDir\..\native_extension\ane\WebViewANE.ane"
--- example-mobile/get_android_dependencies.sh
+++ example-mobile/get_android_dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 wget -O android_dependencies/com.tuarua.frekotlin.ane https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/kotlin/com.tuarua.frekotlin.ane?raw=true
-wget -O ../native_extension/ane/WebViewANE.ane https://github.com/tuarua/WebViewANE/releases/download/1.6.5/WebViewANE.ane?raw=true
+wget -O ../native_extension/ane/WebViewANE.ane https://github.com/tuarua/WebViewANE/releases/download/1.7.0/WebViewANE.ane?raw=true
