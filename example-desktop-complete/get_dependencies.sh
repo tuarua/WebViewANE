5a1510c4d45cc44e810f55c78b98c82e1b9c6176
Eoin Landy <el111@yahoo.com>
2018-07-01 23:27:09 +0100

    1.7.0

--- example-desktop-complete/get_dependencies.sh
+++ example-desktop-complete/get_dependencies.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-wget -O ../native_extension/ane/CommonDependencies.ane https://github.com/tuarua/Swift-IOS-ANE/releases/download/2.4.0/CommonDependencies.ane?raw=true
-wget -O ../native_extension/ane/WebViewANE.ane https://github.com/tuarua/WebViewANE/releases/download/1.7.0/WebViewANE.ane?raw=true
+AneVersion="1.7.0"
+FreSwiftVersion="2.4.0"
+
+wget -O ../native_extension/ane/CommonDependencies.ane https://github.com/tuarua/Swift-IOS-ANE/releases/download/$FreSwiftVersion/CommonDependencies.ane?raw=true
+wget -O ../native_extension/ane/WebViewANE.ane https://github.com/tuarua/WebViewANE/releases/download/$AneVersion/WebViewANE.ane?raw=true
--- example-mobile/get_ios_dependencies.sh
+++ example-mobile/get_ios_dependencies.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
+AneVersion="1.7.0"
+FreSwiftVersion="2.4.0"
+
 rm -r ios_dependencies/device
 rm -r ios_dependencies/simulator
 
-wget https://github.com/tuarua/Swift-IOS-ANE/releases/download/2.4.0/ios_dependencies.zip
+wget https://github.com/tuarua/Swift-IOS-ANE/releases/download/$FreSwiftVersion/ios_dependencies.zip
 unzip -u ios_dependencies.zip
 rm ios_dependencies.zip
 
-wget https://github.com/tuarua/WebViewANE/releases/download/1.7.0/ios_dependencies.zip
+wget https://github.com/tuarua/WebViewANE/releases/download/$AneVersion/ios_dependencies.zip
 unzip -u -o ios_dependencies.zip
 rm ios_dependencies.zip
 
-wget -O ../native_extension/ane/WebViewANE.ane https://github.com/tuarua/WebViewANE/releases/download/1.7.0/WebViewANE.ane?raw=true
+wget -O ../native_extension/ane/WebViewANE.ane https://github.com/tuarua/WebViewANE/releases/download/$AneVersion/WebViewANE.ane?raw=true
