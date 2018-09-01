2e99a5ce091446622c641634a697b76803c25fa5
Eoin Landy <info@tuarua.co.uk>
2018-09-01 18:06:36 +0100

    1.9.0 Android

--- example-mobile/get_android_dependencies.sh
+++ example-mobile/get_android_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-AneVersion="1.8.3"
+AneVersion="1.9.0"
 
 wget -O android_dependencies/com.tuarua.frekotlin.ane https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/kotlin/com.tuarua.frekotlin.ane?raw=true
 wget -O ../native_extension/ane/WebViewANE.ane https://github.com/tuarua/WebViewANE/releases/download/$AneVersion/WebViewANE.ane?raw=true
