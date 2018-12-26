11d44e702506419a71f8dd74b9bb70687185c268
Eoin Landy <info@tuarua.co.uk>
2018-12-26 14:30:02 +0000

    AIR SDK patch

--- example-mobile/get_ios_dependencies.sh
+++ example-mobile/get_ios_dependencies.sh
@@ -10,9 +10,9 @@ wget https://github.com/tuarua/Swift-IOS-ANE/releases/download/$FreSwiftVersion/
 unzip -u ios_dependencies.zip
 rm ios_dependencies.zip
 
-wget https://github.com/tuarua/Swift-IOS-ANE/releases/download/$FreSwiftVersion/AIRSDK_additions.zip
-unzip -u -o AIRSDK_additions.zip
-rm AIRSDK_additions.zip
+wget https://github.com/tuarua/Swift-IOS-ANE/releases/download/$FreSwiftVersion/AIRSDK_patch.zip
+unzip -u -o AIRSDK_patch.zip
+rm AIRSDK_patch.zip
 
 wget https://github.com/tuarua/WebViewANE/releases/download/$AneVersion/ios_dependencies.zip
 unzip -u -o ios_dependencies.zip
