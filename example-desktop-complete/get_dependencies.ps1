ecb1bc7b61fbb78ccb9b364ad20fc615b3b81a5e
Eoin Landy <el111@yahoo.com>
2019-08-05 20:44:31 +0100

    desktop version bump

--- example-desktop-complete/get_dependencies.ps1
+++ example-desktop-complete/get_dependencies.ps1
@@ -1,8 +1,9 @@
-$AneVersion = "2.5.1"
+$AneVersion = "2.6.0"
 $FreSharpVersion = "2.2.0"
+$FreSwiftVersion = "3.1.0"
 
 $currentDir = (Get-Item -Path ".\" -Verbose).FullName
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri https://github.com/tuarua/Swift-IOS-ANE/releases/download/3.1.0/FreSwift.ane?raw=true -OutFile "$currentDir\..\native_extension\ane\FreSwift.ane"
+Invoke-WebRequest -Uri https://github.com/tuarua/Swift-IOS-ANE/releases/download/$FreSwiftVersion/FreSwift.ane?raw=true -OutFile "$currentDir\..\native_extension\ane\FreSwift.ane"
 Invoke-WebRequest -Uri https://github.com/tuarua/WebViewANE/releases/download/$AneVersion/WebViewANE.ane?raw=true -OutFile "$currentDir\..\native_extension\ane\WebViewANE.ane"
 Invoke-WebRequest -Uri https://github.com/tuarua/FreSharp/releases/download/$FreSharpVersion/FreSharp.ane?raw=true -OutFile "$currentDir\..\native_extension\ane\FreSharp.ane"
--- example-desktop-complete/get_dependencies.sh
+++ example-desktop-complete/get_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-AneVersion="2.5.1"
+AneVersion="2.6.0"
 FreSwiftVersion="3.1.0"
 FreSharpVersion="2.2.0"
 
