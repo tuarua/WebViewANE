0cc431c6e4d7806a803bfdc2890fae0fc2b7393a
Eoin Landy <info@tuarua.co.uk>
2020-10-03 18:50:44 +0100

    2.13.0

--- example-desktop-complete/get_dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-AneVersion="2.12.0"
-FreSwiftVersion="4.5.0"
-FreSharpVersion="2.4.0"
-
-wget -O ../native_extension/ane/FreSwift.ane https://github.com/tuarua/Swift-IOS-ANE/releases/download/$FreSwiftVersion/FreSwift.ane?raw=true
-wget -O ../native_extension/ane/WebViewANE.ane https://github.com/tuarua/WebViewANE/releases/download/$AneVersion/WebViewANE.ane?raw=true
-wget -O ../native_extension/ane/FreSharp.ane https://github.com/tuarua/FreSharp/releases/download/$FreSharpVersion/FreSharp.ane?raw=true
